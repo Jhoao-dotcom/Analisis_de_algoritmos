021ac48b0040960c0ef9b60db1d2d1d877f9c414
Jhoaos <jhare24jhoao@gmail.com>
2025-09-16 22:13:55 -0500

    Update EstudiantesFrame.java

--- EstudiantesFrame.java
+++ EstudiantesFrame.java
@@ -13,8 +13,6 @@ import java.util.ArrayList;
  *
  * @author JHARE
  */
-
-
     /**
      */
 // extiende el jframe que es la ventanita
@@ -318,6 +316,7 @@ private ArrayList<Estudiante> mergeRuns(ArrayList<Estudiante> run1, ArrayList<Es
 }
         //acomodar en clases java para una meor organizacion
 //netbeans  b
+
 
 
 
